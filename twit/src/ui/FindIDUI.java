f3b474eb0102f599bdd1e948dbe0412e19a9eda7
eedo_y <doup2001@naver.com>
2023-11-28 22:49:55 +0900

    static DB optimization

--- twit/src/ui/FindIDUI.java
+++ twit/src/ui/FindIDUI.java
@@ -19,6 +19,11 @@ public class FindIDUI extends JFrame {
     private JTextField emailField;
     private JTextField birthdateField;
 
+    // 상수로 DB 연결 정보 정의
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/twit";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "David100894@";
+
     public FindIDUI() {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(500, 80, 400, 450);
@@ -36,29 +41,12 @@ public class FindIDUI extends JFrame {
         lblFindID.setBounds(14, 50, 373, 50);
         contentPane.add(lblFindID);
 
+        // 이름
         JLabel lblFirstName = new JLabel("이름 / First Name");
         lblFirstName.setForeground(new Color(254, 255, 255));
         lblFirstName.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
         lblFirstName.setBounds(72, 120, 130, 27);
         contentPane.add(lblFirstName);
-
-        JLabel lblLastName = new JLabel("성 / Last Name");
-        lblLastName.setForeground(new Color(254, 255, 255));
-        lblLastName.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
-        lblLastName.setBounds(72, 170, 130, 27);
-        contentPane.add(lblLastName);
-
-        JLabel lblEmail = new JLabel("이메일 / Email");
-        lblEmail.setForeground(new Color(254, 255, 255));
-        lblEmail.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
-        lblEmail.setBounds(72, 220, 100, 27);
-        contentPane.add(lblEmail);
-
-        JLabel lblBirthdate = new JLabel("생년월일 / Birthdate");
-        lblBirthdate.setForeground(new Color(254, 255, 255));
-        lblBirthdate.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
-        lblBirthdate.setBounds(72, 270, 130, 27);
-        contentPane.add(lblBirthdate);
 
         firstnameField = new JTextField();
         firstnameField.setFont(new Font("Nanum Gothic", Font.PLAIN, 20));
@@ -66,11 +54,25 @@ public class FindIDUI extends JFrame {
         contentPane.add(firstnameField);
         firstnameField.setColumns(10);
 
+        // 성
+        JLabel lblLastName = new JLabel("성 / Last Name");
+        lblLastName.setForeground(new Color(254, 255, 255));
+        lblLastName.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
+        lblLastName.setBounds(72, 170, 130, 27);
+        contentPane.add(lblLastName);
+
         lastnameField = new JTextField();
         lastnameField.setFont(new Font("Nanum Gothic", Font.PLAIN, 20));
         lastnameField.setBounds(69, 190, 262, 33);
         contentPane.add(lastnameField);
         lastnameField.setColumns(10);
+
+        // 이메일
+        JLabel lblEmail = new JLabel("이메일 / Email");
+        lblEmail.setForeground(new Color(254, 255, 255));
+        lblEmail.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
+        lblEmail.setBounds(72, 220, 100, 27);
+        contentPane.add(lblEmail);
 
         emailField = new JTextField();
         emailField.setFont(new Font("Nanum Gothic", Font.PLAIN, 20));
@@ -78,16 +80,25 @@ public class FindIDUI extends JFrame {
         contentPane.add(emailField);
         emailField.setColumns(10);
 
+        // 생년월일
+        JLabel lblBirthdate = new JLabel("생년월일 / Birthdate");
+        lblBirthdate.setForeground(new Color(254, 255, 255));
+        lblBirthdate.setFont(new Font("Nanum Gothic", Font.PLAIN, 12));
+        lblBirthdate.setBounds(72, 270, 130, 27);
+        contentPane.add(lblBirthdate);
+
         birthdateField = new JTextField();
         birthdateField.setFont(new Font("Nanum Gothic", Font.PLAIN, 20));
         birthdateField.setBounds(69, 290, 262, 33);
         contentPane.add(birthdateField);
         birthdateField.setColumns(10);
 
+        // 아이디 검색하기
         JButton findIDButton = new JButton("아이디 검색하기");
         findIDButton.setBackground(new Color(197, 225, 255));
         findIDButton.setFont(new Font("Nanum Gothic", Font.BOLD, 14));
         findIDButton.setBounds(69, 350, 262, 40);
+        findIDButton.setForeground(new Color(254, 255, 255));
         contentPane.add(findIDButton);
 
         findIDButton.addActionListener(new ActionListener() {
@@ -98,7 +109,8 @@ public class FindIDUI extends JFrame {
                 String birthdate = birthdateField.getText();
 
                 try {
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/twit", "root", "David100894@");
+                    // 상수로 정의한 DB 연결 정보 사용
+                    Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
                     String query = "SELECT id FROM account WHERE firstname=? AND lastname=? AND email=? AND birthdate=?";
                     try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
